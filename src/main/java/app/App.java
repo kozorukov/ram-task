4f27ffe9eccb8c28ede3905257c0b1c682337b96
kozorukov <kosorukov.r@numdes.com>
2023-07-21 12:11:21 +0300

    refactoring

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,10 +7,5 @@ public class App
     public static void main( String[] args )
     {
         var randomAccessMemory = new RandomAccessMemoryImpl(5);
-        randomAccessMemory.setMemory(5);
-        randomAccessMemory.cleanMemory(2, 3);
-        var info = randomAccessMemory.getMemoryAmountInformation();
-        randomAccessMemory.setMemory(2);
-        var a = 1;
     }
 }
